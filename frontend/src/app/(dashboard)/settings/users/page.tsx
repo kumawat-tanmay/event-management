223599e3605eb346e40db249daf8624baf1e77e6
kuldeepkumawat0023 <kuldeepkumawat2383@gmail.com>
2026-08-05 09:32:55 +0530

    feat: add users settings page to dashboard interface

---
+++ frontend/src/app/(dashboard)/settings/users/page.tsx
@@ -0,0 +1,9 @@
+import UsersListView from '@/components/dashboard/settings/UsersListView';
+
+export const metadata = {
+    title: 'My Profile | Krishna Tent & Events',
+};
+
+export default function ProfilePage() {
+    return <UsersListView />;
+}
